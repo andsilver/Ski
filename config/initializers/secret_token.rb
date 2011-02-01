f1fe66c575be03ebc5aef65b8ae88a8fc9515e0a
Elite <elicloudh@gmail.com>
2011-02-01 13:57:15 +0000

    Initial import Rails 3.0.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySkiChalet::Application.config.secret_token = '0bb11ff210a8f3590e3ea23bf1a0b2830e824a4278f4e7130e20cebf025128d0e6191c67b41a0b16798c2f628c1a8d4b7b5d033ea4a3ede05469b43b7e0a0924'
