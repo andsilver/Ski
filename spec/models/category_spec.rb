dc15a3ed511913e3ce09282494945a061a3e041a
Elite <elicloudh@gmail.com>
2011-03-11 14:50:15 +0000

    Spec SEO-friendly to_param for Category and Resort
    eg,
    
    myskichalet.co.uk/resorts/2-italian-alps
    
    instead of
    
    myskichalet.co.uk/resorts/2

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has an SEO-friendly to_param" do
+    category = Category.new
+    category.name = "Bars & Restaurants"
+    category.id = 1
+    category.to_param.should == "1-bars-restaurants"
+  end
 end
--- spec/models/resort_spec.rb
+++ spec/models/resort_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Resort do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has an SEO-friendly to_param" do
+    resort = Resort.new
+    resort.name = "Italian Alps"
+    resort.id = 2
+    resort.to_param.should == "2-italian-alps"
+  end
 end
