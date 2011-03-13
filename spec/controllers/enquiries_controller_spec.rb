ec6077e10f404645bcdb3d6778bb9e21b69ae4ef
Elite <elicloudh@gmail.com>
2011-03-13 23:17:05 +0000

    spec EnquiriesController#create and #current_time

--- spec/controllers/enquiries_controller_spec.rb
+++ spec/controllers/enquiries_controller_spec.rb
@@ -28,4 +28,35 @@ describe EnquiriesController do
       end
     end
   end
+
+  describe "POST create" do
+    let(:property) { mock_model(Property).as_null_object }
+    let(:user) { mock_model(User).as_null_object }
+
+    before do
+      Property.stub(:find).and_return(property)
+      property.stub(:user).and_return(user)
+    end
+
+    it "finds a property" do
+      Property.should_receive(:find).with("1")
+      post :create, :enquiry => { :property_id => "1" }
+    end
+  end
+
+  describe "spam protection" do
+    describe "POST current_time" do
+      let(:time) { Time.mktime(2011) }
+      let(:time_to_i) { time.to_i }
+
+      before do
+        Time.stub(:now).and_return(time)
+      end
+
+      it "sets session[:enquiry_token] with a secretly hashed current time" do
+        post :current_time
+        session[:enquiry_token].should eq(Digest::SHA1.hexdigest("--#{time_to_i}--#{MySkiChalet::Application.config.secret_token}--"))
+      end
+    end
+  end
 end
