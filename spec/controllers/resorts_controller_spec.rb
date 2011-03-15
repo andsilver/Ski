cd032ce67510929121c1aaca2aed1c119a8abec1
Elite <elicloudh@gmail.com>
2011-03-15 17:41:16 +0000

    spec ResortsController#index and #destroy

--- spec/controllers/resorts_controller_spec.rb
+++ spec/controllers/resorts_controller_spec.rb
@@ -3,7 +3,26 @@ require 'spec_helper'
 describe ResortsController do
   let(:resort) { mock_model(Resort).as_null_object }
 
+  before do
+    controller.stub(:admin?).and_return(true)
+  end
+
   describe "GET index" do
+    let(:resorts) { mock(Array) }
+
+    before do
+      Resort.stub(:all).and_return(resorts)
+    end
+
+    it "finds all resorts" do
+      Resort.should_receive(:all)
+      get :index
+    end
+
+    it "assigns @resorts" do
+      get :index
+      assigns[:resorts].should equal(resorts)
+    end
   end
 
   describe "GET info" do
@@ -16,6 +35,19 @@ describe ResortsController do
       Resort.stub(:find_by_id).and_return(resort)
       get :info, :id => "1"
       assigns[:resort].should equal(resort)
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "destroys a resort" do
+      Resort.stub(:find_by_id).and_return(resort)
+      resort.should_receive(:destroy)
+      delete :destroy, :id => "1"
+    end
+
+    it "redirects to the resorts index" do
+      delete :destroy, :id => "1"
+      response.should redirect_to(resorts_path)
     end
   end
 end
