152bed4f8c074ae92272a0f5851ee2f64fdebb56
Elite <elicloudh@gmail.com>
2011-03-16 18:16:53 +0000

    Add parameters (Ruby 1.8 compatibility?)

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -4,7 +4,7 @@ class Advert < ActiveRecord::Base
 
   def self.new_for(object)
     advert = Advert.new
-    advert.send (object.class.to_s + "_id=").downcase.to_sym, object.id
+    advert.send((object.class.to_s + "_id=").downcase.to_sym, object.id)
     advert.user_id = object.user_id
     advert
   end
