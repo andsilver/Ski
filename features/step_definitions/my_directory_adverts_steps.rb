d58d3a3abe05bfd6916ddf0f238567e19750e974
Elite <elicloudh@gmail.com>
2011-03-22 19:10:35 +0000

    Fix creating a directory advert in scenario

--- features/step_definitions/my_directory_adverts_steps.rb
+++ features/step_definitions/my_directory_adverts_steps.rb
@@ -4,7 +4,7 @@ Given /^my business is called "([^"]*)"$/ do |arg1|
 end
 
 Given /^I have directory adverts$/ do
-  DirectoryAdvert.create!(:user_id => alice.id, :category_id => categories(:bars))
+  DirectoryAdvert.create!(:user_id => alice.id, :category_id => categories(:bars).id)
 end
 
 Given /^I have no directory adverts$/ do
