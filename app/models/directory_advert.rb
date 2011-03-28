fcbc1020a9c43cd799de102db297f4d7df7c79b7
Elite <elicloudh@gmail.com>
2011-03-28 13:47:51 +0100

    Price for 9-month directory advert

--- app/models/directory_advert.rb
+++ app/models/directory_advert.rb
@@ -12,7 +12,7 @@ class DirectoryAdvert < ActiveRecord::Base
     user.business_name
   end
 
-  PRICES = { 1 => 1000, 3 => 2700, 6 => 4800, 12 => 8400 }
+  PRICES = { 1 => 1000, 3 => 2700, 6 => 4800, 9 => 6300, 12 => 7200 }
   def price(advert, directory_adverts_so_far)
     PRICES[advert.months]
   end
