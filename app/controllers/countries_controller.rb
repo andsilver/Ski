40bf4811778cc43a1ded826abf4f3bfdd3f90ac6
Elite <elicloudh@gmail.com>
2011-04-04 11:18:27 +0100

    Countries admin

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,5 +1,42 @@
 class CountriesController < ApplicationController
+  before_filter :admin_required, :only => [:index]
+  before_filter :find_country, :only => [:edit, :update, :show]
+
+  def index
+    @countries = Country.all(:order => 'name')
+  end
+
+  def new
+    @country = Country.new
+  end
+
+  def create
+    @country = Country.new(params[:country])
+
+    if @country.save
+      redirect_to(countries_path, :notice => 'Country created.')
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @country.update_attributes(params[:country])
+      redirect_to(countries_path, :notice => 'Saved')
+    else
+      render "edit"
+    end
+  end
+
   def show
+  end
+
+  protected
+
+  def find_country
     @country = Country.find(params[:id])
   end
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -3,6 +3,9 @@ class Country < ActiveRecord::Base
 
   scope :with_resorts, where('id IN (SELECT DISTINCT(country_id) FROM resorts)')
 
+  validates_uniqueness_of :name
+  validates_uniqueness_of :iso_3166_1_alpha_2
+
   def to_s
     name
   end
