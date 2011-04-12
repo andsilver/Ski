e3dc55c774487a59dc13a1f190a5942f58f513b3
Elite <elicloudh@gmail.com>
2011-04-12 12:19:03 +0100

    Order countries by name
    In Countries.with_resorts and resort form

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,7 +1,7 @@
 class Country < ActiveRecord::Base
   has_many :resorts, :order => 'name'
 
-  scope :with_resorts, where('id IN (SELECT DISTINCT(country_id) FROM resorts)')
+  scope :with_resorts, where('id IN (SELECT DISTINCT(country_id) FROM resorts)').order('name')
 
   validates_uniqueness_of :name
   validates_uniqueness_of :iso_3166_1_alpha_2
