0f055677a1e375682602385a74eaf87711b83f94
Elite <elicloudh@gmail.com>
2011-04-26 14:00:36 +0100

    Allow coupons for up to 1M adverts
    To approximate infinity

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,5 +3,5 @@ class Coupon < ActiveRecord::Base
 
   validates_presence_of :code
   validates_uniqueness_of :code
-  validates_inclusion_of :free_adverts, :in => 1..1000, :message => "is not in the range 1-1000"
+  validates_inclusion_of :free_adverts, :in => 1..1000000, :message => "is not in the range 1-1000000"
 end
