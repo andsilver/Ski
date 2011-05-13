03d51c3fdcd2e3b3478fe9bd3ba24cc3b1f720b7
Elite <elicloudh@gmail.com>
2011-05-13 16:00:22 +0100

    Ability to delete countries
    Deletion of countries is only permitted as long
    as the country has no orders, resorts or users
    associated with it.

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,6 +1,6 @@
 class CountriesController < ApplicationController
   before_filter :admin_required, :only => [:index]
-  before_filter :find_country, :only => [:edit, :update, :show]
+  before_filter :find_country, :only => [:edit, :update, :show, :destroy]
 
   def index
     @countries = Country.all(:order => 'name')
@@ -33,6 +33,20 @@ class CountriesController < ApplicationController
 
   def show
     @heading_a = @country.name
+  end
+
+  def destroy
+    @errors = []
+    @errors << "This country has orders associated with it. " unless @country.orders.empty?
+    @errors << "This country has resorts associated with it. " unless @country.resorts.empty?
+    @errors << "This country has users associated with it. " unless @country.users.empty?
+    if @errors.empty?
+      #@country.destroy
+      redirect_to countries_path, :notice => "Country deleted."
+    else
+      redirect_to countries_path, :notice => "This country could not be deleted because: " +
+        @errors.join
+    end
   end
 
   protected
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,8 @@
 class Country < ActiveRecord::Base
   has_many :resorts, :order => 'name'
   has_many :visible_resorts, :class_name => 'Resort', :conditions => 'visible = 1'
+  has_many :orders
+  has_many :users, :foreign_key => 'billing_country_id'
 
   scope :with_resorts, where('id IN (SELECT DISTINCT(country_id) FROM resorts)').order('name')
   scope :with_visible_resorts, where('id IN (SELECT DISTINCT(country_id) FROM resorts WHERE visible=1)').order('name')
