13d450d8d7532d983ee4573e1e0e8181ae54a649
Elite <elicloudh@gmail.com>
2011-05-26 14:41:46 +0100

    Show property counts

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -20,4 +20,23 @@ module PropertiesHelper
     end
     (html + ' ' + label + '</div>').html_safe
   end
+
+  def count_properties_for_rent_in resort
+    count_properties_in resort, :for_sale, 0
+  end
+
+  def count_properties_for_sale_in resort
+    count_properties_in resort, :for_sale, 1
+  end
+
+  def count_new_developments_in resort
+    count_properties_in resort, :new_development, 1
+  end
+
+  def count_properties_in resort, attribute, value
+    @conditions = PropertiesController::CURRENTLY_ADVERTISED.dup
+    @conditions[0] += " AND resort_id = #{resort.id}"
+    @conditions[0] += " AND #{attribute.to_s} = #{value}"
+    "(#{Property.where(@conditions).count})"
+  end
 end
