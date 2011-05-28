85bf44b41dd38317b9c01fcadc71860fc912eda5
Elite <elicloudh@gmail.com>
2011-05-28 18:39:10 +0100

    Remove 'fully equipped kitchen' search filter
    Suggested by Tony in phone conversation

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -21,8 +21,8 @@ class PropertiesController < ApplicationController
       "metres_from_lift ASC", "sleeping_capacity ASC", "number_of_bedrooms ASC" ])
     @conditions[0] += " AND for_sale = 0"
 
-    @search_filters = [:parking, :pets, :smoking, :tv, :satellite, :wifi, :disabled,
-      :fully_equipped_kitchen, :long_term_lets_available, :ski_in_ski_out]
+    @search_filters = [:parking, :pets, :smoking, :tv, :satellite, :wifi,
+      :disabled, :long_term_lets_available, :ski_in_ski_out]
 
     filter_conditions
 
