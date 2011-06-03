26e01844a52eb327fbd771e7b6eaeea3e38c9b2f
Elite <elicloudh@gmail.com>
2011-06-03 12:20:10 +0100

    Prepend category_names. to i18n key

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Category do
   it "has an SEO-friendly to_param using i18n" do
     category = Category.new
-    category.name = "internet_cafe"
+    category.name = "category_names.internet_cafe"
     category.id = 1
     category.to_param.should == "1-internet-cafes"
   end
