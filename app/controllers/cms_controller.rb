6ef6144c49224525f7ed30334a1c27b981a4115a
Elite <elicloudh@gmail.com>
2011-09-21 17:29:19 +0100

    Page titles in CMS

--- app/controllers/cms_controller.rb
+++ app/controllers/cms_controller.rb
@@ -3,13 +3,14 @@ class CmsController < ApplicationController
   before_filter :no_browse_menu
 
   def index
-    @heading_a = t('cms_controller.cms')
+    @page_title = @heading_a = t('cms_controller.cms')
   end
 
   def management_information
+    @page_title = @heading_a = t('cms_controller.management_information')
   end
 
   def gross_sales_analysis
-    @heading_a = t('cms_controller.gross_sales_analysis')
+    @page_title = @heading_a = t('cms_controller.gross_sales_analysis')
   end
 end
