691f534545ca0235ff57944261c11e384717d00c
Elite <elicloudh@gmail.com>
2011-09-22 13:46:39 +0100

    spec Role

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,65 @@
 require 'spec_helper'
 
 describe Role do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#only_advertises_properties_for_rent?" do
+    it "only returns true when there are no other advertising options" do
+      role = Role.new
+      role.advertises_properties_for_rent = true
+      role.advertises_properties_for_sale = false
+      role.advertises_generally = false
+      role.only_advertises_properties_for_rent?.should be_true
+
+      role.advertises_properties_for_rent = false
+      role.only_advertises_properties_for_rent?.should be_false
+
+      role.advertises_properties_for_rent = true
+      role.advertises_properties_for_sale = true
+      role.only_advertises_properties_for_rent?.should be_false
+
+      role.advertises_properties_for_sale = true
+      role.advertises_generally = true
+      role.only_advertises_properties_for_rent?.should be_false
+    end
+  end
+
+  describe "#only_advertises_properties_for_sale?" do
+    it "only returns true when there are no other advertising options" do
+      role = Role.new
+      role.advertises_properties_for_sale = true
+      role.advertises_properties_for_rent = false
+      role.advertises_generally = false
+      role.only_advertises_properties_for_sale?.should be_true
+
+      role.advertises_properties_for_sale = false
+      role.only_advertises_properties_for_sale?.should be_false
+
+      role.advertises_properties_for_sale = true
+      role.advertises_properties_for_rent = true
+      role.only_advertises_properties_for_sale?.should be_false
+
+      role.advertises_properties_for_rent = true
+      role.advertises_generally = true
+      role.only_advertises_properties_for_sale?.should be_false
+    end
+  end
+
+  describe "#localisation_key" do
+    it "prepends 'roles.'" do
+      role = Role.new
+      role.name = "admin"
+      role.localisation_key.should =="roles.admin"
+    end
+
+    it "downcases letters" do
+      role = Role.new
+      role.name = "Admin"
+      role.localisation_key.should =="roles.admin"
+    end
+
+    it "substitutes spaces with underscores" do
+      role = Role.new
+      role.name = "letting agent"
+      role.localisation_key.should =="roles.letting_agent"
+    end
+  end
 end
