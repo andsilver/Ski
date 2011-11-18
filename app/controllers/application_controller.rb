992c12f4dc0e4cf8edc87af0b9305a900e0bc95b
Elite <elicloudh@gmail.com>
2011-11-18 17:47:44 +0000

    Don't show fixed banners when filename is empty

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,8 +33,10 @@ class ApplicationController < ActionController::Base
       @page_title = page.title
       @meta_description = page.description
       @meta_keywords = page.keywords
-      @fixed_banner_image_path = "/fixed-banners/" + page.fixed_banner_image_filename
-      @fixed_banner_target_url = page.fixed_banner_target_url
+      unless page.fixed_banner_image_filename.blank?
+        @fixed_banner_image_path = "/fixed-banners/" + page.fixed_banner_image_filename
+        @fixed_banner_target_url = page.fixed_banner_target_url
+      end
     end
   end
 
