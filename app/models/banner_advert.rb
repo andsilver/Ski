1fe434e44949bd66cd0f5357c40def6e8c0260d3
Elite <elicloudh@gmail.com>
2012-01-15 18:24:09 +0000

    Remove BannerAdvert#adverts_for

--- app/models/banner_advert.rb
+++ app/models/banner_advert.rb
@@ -10,25 +10,6 @@ class BannerAdvert < ActiveRecord::Base
     save
   end
 
-  def self.adverts_for(resort, dimensions, qty)
-    return [] if(rand > 0.7)
-
-    conditions = CURRENTLY_ADVERTISED.dup
-    conditions[0] += " AND resort_id = ? AND width = ? AND height = ?"
-    conditions[0] += " AND image_id IS NOT NULL"
-    conditions << resort.id
-    conditions << dimensions[0]
-    conditions << dimensions[1]
-
-    ads = []
-    uncached do
-      ads = BannerAdvert.all(:order => 'RAND()', :conditions => conditions, :limit => qty)
-    end
-
-    ads.each {|ad| ad.current_advert.record_view}
-    ads
-  end
-
   def self.small_banners_for(resort, qty = 3)
     self.adverts_for(resort, [160, 200], qty)
   end
