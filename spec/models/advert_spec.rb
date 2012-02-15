8fc2674da0277ac7d84160e111d1f606073bedcc
Elite <elicloudh@gmail.com>
2012-02-15 18:51:49 +0000

    Describe Advert#price

--- spec/models/advert_spec.rb
+++ spec/models/advert_spec.rb
@@ -6,4 +6,29 @@ describe Advert do
       Advert.new_for(Property.new({:user_id => 1})).is_a?(Advert).should be_true
     end
   end
+
+  describe "#price" do
+    context "when it has an object" do
+      let(:property) { mock_model(Property).as_null_object }
+
+      it "returns the object's price" do
+        price_of_property = 99
+        a = valid_advert
+        a.stub(:object).and_return(property)
+        property.should_receive(:price).with(a, 1).and_return(price_of_property)
+        a.price(1).should equal(price_of_property)
+      end
+    end
+
+    context "when it has no object" do
+      it "returns 0" do
+        a = valid_advert
+        a.price(1).should equal(0)
+      end
+    end
+  end
+
+  def valid_advert
+    Advert.new(:user_id => 1)
+  end
 end
