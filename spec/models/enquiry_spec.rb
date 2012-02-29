09ac45e1870faba53deb18f325b759c0c923e207
Elite <elicloudh@gmail.com>
2012-02-29 18:45:34 +0000

    Simplify validation tests

--- spec/models/enquiry_spec.rb
+++ spec/models/enquiry_spec.rb
@@ -1,16 +1,13 @@
 require 'spec_helper'
 
 describe Enquiry do
+  # ActiveModel
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:phone) }
+
   def valid_enquiry
     Enquiry.new(:name => "Alice", :email => "alice@mychaletfinder.com",
       :phone => "01234 567890")
-  end
-
-  it "validates presence of name" do
-    e = valid_enquiry
-    e.should be_valid
-    e.name = ""
-    e.should_not be_valid
   end
 
   it "validates format of email" do
@@ -19,13 +16,6 @@ describe Enquiry do
     e.email = ""
     e.should_not be_valid
     e.email = "a@b"
-    e.should_not be_valid
-  end
-
-  it "validates presence of phone" do
-    e = valid_enquiry
-    e.should be_valid
-    e.phone = ""
     e.should_not be_valid
   end
 end
