14c1b36c114a2d2f1015e102715f0e80863cf773
Elite <elicloudh@gmail.com>
2012-05-18 10:43:21 +0100

    Don't verify certificates

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 MySkiChalet::Application.initialize!
+
+# Don't verify certificates
+ActionMailer::Base.smtp_settings = {
+  openssl_verify_mode: 'none'
+}
