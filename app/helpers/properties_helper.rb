1982098cfeac9120dedcb8ae8868c40c6e2054ee
Elite <elicloudh@gmail.com>
2012-06-07 08:20:19 +0100

    Tidy search options and property summaries

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -3,7 +3,7 @@ module PropertiesHelper
     html = ''
     filters.each do |f|
       param = 'filter_' + f.to_s
-      html += '<label class="filter"><input'
+      html += '<label class="checkbox"><input'
       html += ' checked' if params[param]=='on'
       html += ' type="checkbox" name="filter_' + f.to_s + '" onchange="this.form.submit()">'
       html += I18n.t('properties.filters.' + f.to_s) + '</label>'
