419f551c1cf11463e7812dd73a25bfa3f5701281
Elite <elicloudh@gmail.com>
2012-06-21 15:23:39 +0100

    Custom headers for blog posts

--- app/helpers/resorts_helper.rb
+++ app/helpers/resorts_helper.rb
@@ -4,6 +4,7 @@ module ResortsHelper
   PISTE_MAP_SIZE = 821
   COUNTRIES_DIRECTORY = "#{Rails.root.to_s}/public/countries/"
   RESORTS_DIRECTORY = "#{Rails.root.to_s}/public/resorts/"
+  BLOG_POSTS_DIRECTORY = "#{Rails.root.to_s}/public/blog-posts/"
 
   def gallery_thumbnail(resort, filename)
     resort_image(resort, filename, THUMBNAIL_SIZE, 'gallery')
@@ -61,6 +62,14 @@ module ResortsHelper
       end
     end
 
+    if controller.controller_name == 'blog_posts' && controller.action_name == 'show'
+      id = params[:id].to_i.to_s
+      blog_post_images(id)
+      @images.each do |img|
+        urls << "/blog-posts/#{id}/#{img}"
+      end
+    end
+
     if urls.empty? && @country
       if @country.image
         urls << @country.image.url
@@ -86,6 +95,11 @@ module ResortsHelper
 
   def country_images(country, sub_dir)
     dir = "#{COUNTRIES_DIRECTORY}#{country.name.parameterize}/#{sub_dir}"
+    images_in_directory(dir)
+  end
+
+  def blog_post_images(sub_dir)
+    dir = "#{BLOG_POSTS_DIRECTORY}#{sub_dir}"
     images_in_directory(dir)
   end
 
