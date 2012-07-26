b23142e3c0c0b76eb7773e1176dd7f0e3b693840
Elite <elicloudh@gmail.com>
2012-07-26 11:28:17 +0100

    Scope uniqueness of name to locale

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -3,5 +3,5 @@ class Snippet < ActiveRecord::Base
 
   validates_inclusion_of :locale, in: ['de', 'en', 'fr', 'it']
   validates_presence_of :name
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, scope: :locale
 end
