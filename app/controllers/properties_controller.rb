abff345ab8339f2ef3aa4bf81c6106b406996de7
Elite <elicloudh@gmail.com>
2012-08-28 15:59:05 +0100

    Leading 0 for check in day

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -136,7 +136,7 @@ class PropertiesController < ApplicationController
 
   def check_interhome_booking
     @accommodation = InterhomeAccommodation.find_by_permalink(params[:permalink])
-    check_in = params[:interhome_booking][:arrival_month] + '-' + params[:interhome_booking][:arrival_day]
+    check_in = params[:interhome_booking][:arrival_month] + '-' + '%02d' % params[:interhome_booking][:arrival_day]
     check_in_date = Date.new(check_in[0..3].to_i, check_in[5..6].to_i, check_in[8..9].to_i)
     check_out = (check_in_date + params[:interhome_booking][:duration].to_i.days).to_s
     details = {
