8aa2884b2dde814ea98cd8a4f8b8f9246951f5ba
Elite <elicloudh@gmail.com>
2013-03-28 10:20:09 +0000

    Don't use a resort already present in fixtures

--- spec/views/countries/show.html.erb_spec.rb
+++ spec/views/countries/show.html.erb_spec.rb
@@ -6,11 +6,11 @@ describe 'countries/show' do
   it "displays the country's visible resorts in the quick search form" do
     france = countries(:france)
     france.resorts << Resort.create!(name: 'Avoriaz', visible: true)
-    france.resorts << Resort.create!(name: 'Chamonix', visible: false)
+    france.resorts << Resort.create!(name: 'Tignes', visible: false)
     assign(:country, france)
     assign(:featured_properties, [])
     render
     rendered.should have_content('Avoriaz')
-    rendered.should_not have_content('Chamonix')
+    rendered.should_not have_content('Tignes')
   end
 end
