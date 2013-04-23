dcda428639900b8bd17122e371e96d8262e7e4fc
Elite <elicloudh@gmail.com>
2013-04-23 19:59:05 +0100

    Fix: uninitialized constant ActionController::UnknownAction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,6 @@ class ApplicationController < ActionController::Base
     rescue_from ActiveRecord::RecordNotFound, with: :not_found
     rescue_from ActionController::RoutingError, with: :not_found
     rescue_from ActionController::UnknownController, with: :not_found
-    rescue_from ActionController::UnknownAction, with: :not_found
   end
 
   def routing_error
