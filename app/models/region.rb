fc62e803674f86bb453eb291f9e34b417cfeb1da
Elite <elicloudh@gmail.com>
2013-05-22 19:29:10 +0100

    Validate presence of country_id

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,5 +1,6 @@
 class Region < ActiveRecord::Base
   belongs_to :country, inverse_of: :regions
 
+  validates :country_id, presence: true
   validates :name, length: { maximum: 100 }, presence: true, uniqueness: { scope: :country }
 end
