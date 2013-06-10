cc28fc280daed56a0dae125d05b5a14aaa4f62cc
Elite <elicloudh@gmail.com>
2013-06-10 16:20:24 +0100

    Validations for resort holiday types

--- app/models/resort_holiday_type.rb
+++ app/models/resort_holiday_type.rb
@@ -1,4 +1,7 @@
 class ResortHolidayType < ActiveRecord::Base
   belongs_to :resort
   belongs_to :holiday_type
+
+  validates :resort, presence: true, uniqueness: { scope: :holiday_type }
+  validates :holiday_type, presence: true
 end
