f4f21d8f42e425c1ac39649d940302c206260bde
Elite <elicloudh@gmail.com>
2013-06-11 11:46:22 +0100

    Link to resorts from region page

--- spec/features/regions_spec.rb
+++ spec/features/regions_spec.rb
@@ -4,7 +4,13 @@ feature 'Regions' do
   fixtures :regions, :websites
 
   scenario 'Region page shows region info' do
-    visit region_path(regions(:rhones_alpes))
+    visit region_path(regions(:rhone_alpes))
     expect(page).to have_content('Located on the eastern border...')
+  end
+
+  scenario 'Region page with resorts shows a list of resorts' do
+    visit region_path(regions(:rhone_alpes))
+    expect(page.find('#links-and-search')).to have_content 'Resorts'
+    expect(page.find('#links-and-search')).to have_content 'Chamonix'
   end
 end
