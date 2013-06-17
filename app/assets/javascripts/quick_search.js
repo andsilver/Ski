3983287c987e131a99467568879ac6b6829009a1
Elite <elicloudh@gmail.com>
2013-06-17 16:17:57 +0100

    Move quick search JavaScript to its own file

---
+++ app/assets/javascripts/quick_search.js
@@ -0,0 +1,31 @@
+function getQuickSearchResorts() {
+  var valuesToSubmit = $('#quick-search form').serialize();
+  $.ajax({
+    url: '/home/resort_options_for_quick_search',
+    type: 'GET',
+    data: valuesToSubmit
+  }).success(function(data) {
+    $('#resort_id').replaceWith(data);
+
+    if($.cookie('quick_search_resort_id') != null) {
+      $('#resort_id').val($.cookie('quick_search_resort_id'));
+      $.removeCookie('quick_search_resort_id');
+    }
+  });
+  return false;
+}
+
+$(function() {
+  if($('#quick-search').length) {    
+    $('#country_id').change(getQuickSearchResorts);
+    $('#quick-search form').submit(function() {
+      if($('#resort_id').val() == '') {
+        alert('Please choose a resort first.');
+        return false;
+      }
+    });
+    if($('#country_id').val() != '') {
+      getQuickSearchResorts();
+    }
+  }
+});
