c3c0887ff60fc92180a45c7029c9b9400cfad94a
Elite <elicloudh@gmail.com>
2013-07-02 11:25:05 +0100

    404 when region not found

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,5 +1,6 @@
 class RegionsController < ApplicationController
   def show
     @region = Region.find_by(slug: params[:id])
+    not_found unless @region
   end
 end
---
+++ spec/controllers/regions_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe RegionsController do
+  let(:website) { mock_model(Website).as_null_object }
+  let(:region) { mock_model(Region).as_null_object }
+
+  before do
+    Website.stub(:first).and_return(website)
+    controller.stub(:admin?).and_return(false)
+  end
+
+  describe 'GET show' do
+    it 'finds a region by its slug' do
+      Region.should_receive(:find_by).with(slug: 'lake-como')
+      get :show, id: 'lake-como'
+    end
+
+    it 'assigns @region' do
+      Region.stub(:find_by).and_return(region)
+      get :show, id: 'lake-como'
+      expect(assigns[:region]).to equal(region)
+    end
+
+    context 'when region not found' do
+      it 'renders 404' do
+        get :show, id: 'lake-como'
+        expect(response.status).to eq 404
+      end
+    end
+  end
+end
