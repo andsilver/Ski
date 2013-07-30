4cd8122c9b072b19d09f58089ae0ad1cd730f08f
Elite <elicloudh@gmail.com>
2013-07-30 15:23:21 +0100

    Don't show region menu box when no regions resolves #42
    Invisible regions are also not show

--- app/models/country.rb
+++ app/models/country.rb
@@ -5,6 +5,7 @@ class Country < ActiveRecord::Base
   belongs_to :image, dependent: :destroy
 
   has_many :regions, -> { order 'name' }, inverse_of: :country
+  has_many :visible_regions, -> { where(visible: true).order('name') }, class_name: 'Region'
   has_many :resorts, -> { order 'name' }
   has_many :visible_resorts, -> { where(visible: true).order('name') }, class_name: 'Resort'
   has_many :orders
--- spec/views/countries/show.html.erb_spec.rb
+++ spec/views/countries/show.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'countries/show' do
-  fixtures :countries
+  fixtures :countries, :regions
 
   it "displays the country's visible resorts in the quick search form" do
     france = countries(:france)
@@ -12,5 +12,44 @@ describe 'countries/show' do
     render
     expect(view.content_for(:links_and_search)).to have_content('Avoriaz')
     expect(view.content_for(:links_and_search)).to_not have_content('Tignes')
+  end
+
+  context 'with regions' do
+    let(:country) { countries(:france) }
+
+    it 'displays a regions section' do
+      assign(:country, country)
+      assign(:featured_properties, [])
+      render
+      expect(view.content_for(:links_and_search)).to have_content('Regions')
+    end
+  end
+
+  context 'with only invisible regions' do
+    before do
+      @country = FactoryGirl.create(:country)
+      region = FactoryGirl.create(:region, country: @country, visible: false)
+    end
+
+    let(:country) { @country }
+
+    it 'does not display a regions section' do
+      expect_no_regions_section
+    end
+  end
+
+  context 'without regions' do
+    let(:country) { FactoryGirl.create(:country) }
+
+    it 'does not display a regions section' do
+      expect_no_regions_section
+    end
+  end
+
+  def expect_no_regions_section
+    assign(:country, country)
+    assign(:featured_properties, [])
+    render
+    expect(view.content_for(:links_and_search)).to_not have_content('Regions')
   end
 end
