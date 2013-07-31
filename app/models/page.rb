0d60b037889c06ac439efede97917f6260ebc22a
Elite <elicloudh@gmail.com>
2013-07-31 12:28:20 +0100

    Fix page path uniqueness validation

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,7 @@
 class Page < ActiveRecord::Base
   validates :path, :title, presence: true
 
-  validate :path, uniqueness: true
+  validates :path, uniqueness: true
 
   validates :description, :path, :title, length: { maximum: 255 }
 
