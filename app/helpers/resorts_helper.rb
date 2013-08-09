f44b9641a29cad7bb2db17dc0efbf96a9cabe20a
Elite <elicloudh@gmail.com>
2013-08-09 14:54:40 +0100

    Right align advert counts and refactor; resolves #37

--- app/helpers/resorts_helper.rb
+++ app/helpers/resorts_helper.rb
@@ -116,4 +116,12 @@ module ResortsHelper
   def pv_place_codes
     ActiveRecord::Base.connection.execute("SELECT DISTINCT(CONCAT_WS('-', iso_3166_1, iso_3166_2, onu)) FROM `pv_accommodations`").map{|c| c[0]}.sort
   end
+
+  def resort_link_with_count(path, title, link_text, count)
+    return if count == 0
+
+    opts = current_page?(path) ? {class: 'active'} : {}
+
+    content_tag(:li, link_to(h(link_text) + content_tag(:span, "(#{count})"), path, title: title), opts)
+  end
 end
