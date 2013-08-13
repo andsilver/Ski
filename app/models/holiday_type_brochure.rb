b0e5305eac8fdaeeb2e64d2dc34337594b8db009
Elite <elicloudh@gmail.com>
2013-08-13 17:53:06 +0100

    Ensure resort of property also matches holiday type #68

--- app/models/holiday_type_brochure.rb
+++ app/models/holiday_type_brochure.rb
@@ -9,5 +9,14 @@ class HolidayTypeBrochure < ActiveRecord::Base
     Property.order('RAND()')
       .limit(9)
       .where(publicly_visible: true, brochurable_type.foreign_key.to_sym => brochurable.id)
+      .where(resort_matches_holiday_type)
+  end
+
+  def resort_matches_holiday_type
+    if ['Region', 'Country'].include? brochurable_type
+      ['resort_id IN (SELECT brochurable_id FROM holiday_type_brochures WHERE holiday_type_id = ? AND brochurable_type = "Resort")', holiday_type.id]
+    else
+      {}
+    end
   end
 end
