9719959b62f154f26172ca31193dca3fe7c2da89
Elite <elicloudh@gmail.com>
2013-08-23 12:14:49 +0100

    Order region's resorts by name

--- app/models/region.rb
+++ app/models/region.rb
@@ -3,7 +3,7 @@ class Region < ActiveRecord::Base
   include RelatedPages
 
   belongs_to :country, inverse_of: :regions
-  has_many :resorts, inverse_of: :region, dependent: :nullify
+  has_many :resorts, -> { order 'name' }, inverse_of: :region, dependent: :nullify
 
   validates :country_id, presence: true
   validates :name, length: { maximum: 100 }, presence: true, uniqueness: { scope: :country }
