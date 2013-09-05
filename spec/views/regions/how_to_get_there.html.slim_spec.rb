661e845c630522aef8b60bfa24e5df8e0ef2fa73
Elite <elicloudh@gmail.com>
2013-09-05 16:52:30 +0100

    Heading no longer displayed on region how-to-get-there
    This is fine as it allows a richer heading to be authored in the CMS

--- spec/views/regions/how_to_get_there.html.slim_spec.rb
+++ spec/views/regions/how_to_get_there.html.slim_spec.rb
@@ -1,16 +1,11 @@
 require 'spec_helper'
 
 describe 'regions/how_to_get_there' do
-  fixtures :regions
+  fixtures :countries, :regions
 
   before do
     rhone_alpes = regions(:rhone_alpes)
     assign(:region, rhone_alpes)
-  end
-
-  it 'displays a heading' do
-    render
-    expect(Capybara.string(rendered).find('h2')).to have_content(t 'regions.how_to_get_there.heading')
   end
 
   it 'displays the page content' do
