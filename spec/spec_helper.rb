7e66c33a0772bc96648f4c9d310589ac18090976
Elite <elicloudh@gmail.com>
2013-09-12 09:48:33 +0100

    Specs fail fast

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,8 @@ Spork.prefork do
   ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
   RSpec.configure do |config|
+    config.fail_fast = true
+
     # ## Mock Framework
     #
     # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
