67424cc0594e95aa6a3b4b0169dd4cf87cc7d0f8
Elite <elicloudh@gmail.com>
2013-09-12 12:17:09 +0100

    Move price into summary header, closes #94

--- spec/views/properties/_property_summary.html.erb_spec.rb
+++ spec/views/properties/_property_summary.html.erb_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe 'properties/_property_summary' do
+  include ViewSpecHelpers
+
   let(:property) { FactoryGirl.create(:property, metres_from_lift: 500).decorate }
 
   context 'when sorting by distance from lift' do
@@ -16,6 +18,20 @@ describe 'properties/_property_summary' do
     it 'does not display distance from lift' do
       render 'properties/property_summary', p: property
       expect(rendered).to_not have_content('500m')
+    end
+  end
+
+  context 'when for rent' do
+    before do
+      property.listing_type = Property::LISTING_TYPE_FOR_RENT
+      property.weekly_rent_price = 1500
+    end
+
+    it 'displays price in the heading' do
+      render 'properties/property_summary', p: property
+      within 'h3' do |h3|
+        expect(h3).to have_content 'Weekly price from 1,500'
+      end
     end
   end
 end
