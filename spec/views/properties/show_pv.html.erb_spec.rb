5a8e195573bab6c526468f7dcc2db8fd609c5dad
Elite <elicloudh@gmail.com>
2013-09-16 17:39:43 +0100

    Assign @breadcrumbs

--- spec/views/properties/show_pv.html.erb_spec.rb
+++ spec/views/properties/show_pv.html.erb_spec.rb
@@ -6,6 +6,7 @@ describe 'properties/show_pv' do
     pv_accommodation = PvAccommodation.new(price_table_url: 'http://example.org')
     assign(:property, property)
     assign(:accommodation, pv_accommodation)
+    assign(:breadcrumbs, {})
     render
     expect(rendered).to have_selector('iframe#pv-booking[src="http://example.org"]')
   end
