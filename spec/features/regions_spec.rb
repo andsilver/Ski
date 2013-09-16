2a72bfb49eb2c06a0c99d53b74ca8227b3aa81c8
Elite <elicloudh@gmail.com>
2013-09-16 17:57:50 +0100

    Prefer FactoryGirl over fixtures; include missing country association

--- spec/features/regions_spec.rb
+++ spec/features/regions_spec.rb
@@ -1,15 +1,19 @@
 require 'spec_helper'
 
 feature 'Regions' do
-  fixtures :regions, :resorts, :websites
+  fixtures :websites
 
   scenario 'Region page shows region info' do
-    visit region_path(regions(:rhone_alpes))
+    rhone_alpes = FactoryGirl.create(:region, info: 'Located on the eastern border...')
+    visit region_path(rhone_alpes)
     expect(page).to have_content('Located on the eastern border...')
   end
 
   scenario 'Region page with resorts shows a list of resorts' do
-    visit region_path(regions(:rhone_alpes))
+    rhone_alpes = FactoryGirl.create(:region)
+    rhone_alpes.resorts << FactoryGirl.create(:resort, name: 'Chamonix')
+
+    visit region_path(rhone_alpes)
     expect(page.find('#links-and-search')).to have_content 'Resorts'
     expect(page.find('#links-and-search')).to have_content 'Chamonix'
   end
