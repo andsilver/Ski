eb7f9e43de2e4319a51b6b6daca68ec93babf1f8
Elite <elicloudh@gmail.com>
2013-09-16 17:36:22 +0100

    Don't depend on a hardcoded email address

--- spec/features/price_override_spec.rb
+++ spec/features/price_override_spec.rb
@@ -5,8 +5,8 @@ feature 'Price Override' do
   fixtures :banner_prices, :categories, :countries, :resorts, :websites
 
   scenario 'Price override shows in basket' do
-    FactoryGirl.create(:user, {apply_price_override: true, price_override: 500})
-    sign_in_with('zach.anyman@example.org', 'secret')
+    user = FactoryGirl.create(:user, {apply_price_override: true, price_override: 500})
+    sign_in_with(user.email, 'secret')
     add_banner_advert_to_basket
     visit '/basket'
     expect(page).to have_content 'Price override €500'
