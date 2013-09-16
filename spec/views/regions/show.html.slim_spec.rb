27f6fe916634b0eecc61e07cd2b705b10e490818
Elite <elicloudh@gmail.com>
2013-09-16 18:03:53 +0100

    Add quick search to region pages; closes #140

---
+++ spec/views/regions/show.html.slim_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'regions/show.html.slim' do
+  before do
+    assign(:featured_properties, [])
+  end
+
+  it 'shows the quick search form' do
+    assign(:region, FactoryGirl.create(:region))
+    render
+    expect(view.content_for(:links_and_search)).to have_content(t('home.quick_search.heading'))
+  end
+end
