50d56fa144be0da8ef50b4a48da3d08b67093aff
Elite <elicloudh@gmail.com>
2014-01-10 16:31:08 +0000

    Admin UI to change property listing type

--- spec/views/properties/_form.html.erb_spec.rb
+++ spec/views/properties/_form.html.erb_spec.rb
@@ -6,6 +6,13 @@ describe 'properties/_form' do
   context 'with a property developer' do
     before do
       assign(:current_user, users(:bob)) # a property developer
+      view.stub(:admin?).and_return(false)
+    end
+
+    it 'renders listing_type as a hidden field' do
+      assign(:property, Property.new)
+      render
+      expect(rendered).to have_selector('input[type="hidden"]#property_listing_type')
     end
 
     context 'with a property for sale' do
@@ -28,6 +35,19 @@ describe 'properties/_form' do
       it 'does not show a new development checkbox' do
         expect(rendered).not_to have_selector('input#property_new_development')
       end
+    end
+  end
+
+  context 'when admin' do
+    before do
+      view.stub(:admin?).and_return(true)
+      assign(:current_user, users(:tony)) # an administrator
+    end
+
+    it 'renders listing_type as a select field' do
+      assign(:property, Property.new)
+      render
+      expect(rendered).to have_selector('select#property_listing_type')
     end
   end
 end
