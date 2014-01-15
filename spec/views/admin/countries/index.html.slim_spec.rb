e7a670beda14a73a2f8b4f1315d3f0404ccab220
Elite <elicloudh@gmail.com>
2014-01-15 13:58:38 +0000

    Fix link to delete country

---
+++ spec/views/admin/countries/index.html.slim_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'admin/countries/index' do
+  let(:country) { FactoryGirl.create(:country) }
+
+  before do
+    assign(:countries, [country])
+  end
+
+  it 'renders' do
+    render
+  end
+end
