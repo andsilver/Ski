7c5a7c6a117a324a99b460d0efa542d5bd4e4078
Elite <elicloudh@gmail.com>
2014-03-21 18:27:41 +0000

    Fix URLs in window base price admin

--- app/models/window_base_price.rb
+++ app/models/window_base_price.rb
@@ -1,3 +1,7 @@
 class WindowBasePrice < ActiveRecord::Base
   validates_uniqueness_of :quantity
+
+  def to_s
+    "#{quantity} @ #{price}"
+  end
 end
---
+++ spec/features/window_base_prices_admin_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+feature 'Window base prices admin' do
+  fixtures :roles, :users, :websites
+
+  scenario 'List window base prices' do
+    FactoryGirl.create(:window_base_price, price: 95)
+    sign_in_as_admin
+    visit admin_window_base_prices_path
+    expect(page).to have_content '95'
+  end
+
+  scenario 'New window base price' do
+    sign_in_as_admin
+    visit admin_window_base_prices_path
+    click_link 'New'
+    fill_in 'Quantity',   with: '1234'
+    fill_in 'Price',      with: '30'
+    click_button 'Create Window base price'
+    expect(page).to have_content I18n.t('notices.created')
+    expect(page).to have_content '1234'
+  end
+
+  scenario 'Edit window base price' do
+    wbp = FactoryGirl.create(:window_base_price, price: 95)
+    sign_in_as_admin
+    visit admin_window_base_prices_path
+    click_link "Edit #{wbp}"
+    fill_in 'Price', with: '96'
+    click_button 'Update Window base price'
+    expect(page).to have_content I18n.t('notices.saved')
+    expect(page).to have_content '96'
+  end
+end
