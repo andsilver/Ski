77d6d69b4b8de1460b025f3d97eed4702cbc7a71
Elite <elicloudh@gmail.com>
2014-07-10 16:26:18 +0100

    Crude calendar

--- app/models/flip_key_property.rb
+++ app/models/flip_key_property.rb
@@ -1,6 +1,10 @@
 class FlipKeyProperty < ActiveRecord::Base
   has_one :property, dependent: :destroy
 
+  def booked_on?(date)
+    parsed_json['property_calendar'][0]['booked_date'].any? { |bd| bd == date.to_s }
+  end
+
   def parsed_json
     @parsed_json ||= JSON.parse(json_data)
   end
