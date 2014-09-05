22f4c062e8d0daeb2b258d00d5755e7e7520444c
Elite <elicloudh@gmail.com>
2014-09-05 17:24:44 +0100

    Delayed job for background work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140807193117) do
+ActiveRecord::Schema.define(version: 20140905162234) do
 
   create_table "adverts", force: true do |t|
     t.integer  "user_id",                             null: false
@@ -158,6 +158,22 @@ ActiveRecord::Schema.define(version: 20140807193117) do
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
   end
+
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "directory_adverts", force: true do |t|
     t.integer  "user_id",                          null: false
