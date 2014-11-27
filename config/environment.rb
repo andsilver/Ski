327a653003b6d522f957290907b6782d5c84b475
Elite <elicloudh@gmail.com>
2014-11-27 11:50:49 +0000

    Probably don't need this now

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Don't verify certificates.
-ActionMailer::Base.smtp_settings = {
-  openssl_verify_mode: 'none'
-}
-
 # Use Active Record for Liquid templates.
 Liquid::Template.file_system = SnippetFileSystem.new
