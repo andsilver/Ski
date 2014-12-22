68214d317586979a4a606c73572959a92392ac87
Elite <elicloudh@gmail.com>
2014-12-22 15:05:27 +0000

    Run the P&V tidy routine

--- denormalize.sh
+++ denormalize.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cd ~/railsapps/myskichalet && bundle exec rake RAILS_ENV=production denormalize
+cd ~/railsapps/myskichalet && bundle exec rake RAILS_ENV=production pierre_et_vacances:tidy
