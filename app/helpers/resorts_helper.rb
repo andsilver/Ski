afc83b7251cdefbc19ef6322cc4be59bc5aab51f
Elite <elicloudh@gmail.com>
2015-01-05 19:23:19 +0000

    Region header images for how-to-get-there

--- app/helpers/resorts_helper.rb
+++ app/helpers/resorts_helper.rb
@@ -43,30 +43,56 @@ module ResortsHelper
     url.gsub(' ', '%20')
   end
 
-  # TODO: Refactor
+  # Returns header image URLs for the current page.
   def header_image_urls
     urls = []
 
     return urls if @no_header
 
+    [
+      :resort_header_urls,
+      :page_header_urls,
+      :region_header_urls,
+      :country_header_urls
+    ].each do |source|
+      if urls.empty?
+        urls = send(source) || []
+      end
+    end
+
+    urls.map! {|u| u.gsub(' ', '%20')}
+    urls
+  end
+
+  def resort_header_urls
     if @resort
       sub_dir = header_image_sub_directory
       resort_images(@resort, sub_dir)
-
-      @images.each do |img|
-        urls << "/resorts/#{@resort.name.parameterize}/#{sub_dir}/#{img}"
-      end
+        .map {|img| "/resorts/#{@resort.name.parameterize}/#{sub_dir}/#{img}"}
     end
+  end
 
+  def page_header_urls
     if controller.controller_name == 'pages' && controller.action_name == 'show'
       slug = params[:id]
-      @images = images_in_directory("#{Rails.root.to_s}/public/pages/#{slug}/headers")
-      @images.each do |img|
-        urls << "/pages/#{slug}/headers/#{img}"
-      end
+      images_in_directory("#{Rails.root.to_s}/public/pages/#{slug}/headers")
+        .map {|img| "/pages/#{slug}/headers/#{img}"}
     end
+  end
 
-    if urls.empty? && @country
+  # Returns an array of URLs for header images for use in region pages. Only
+  # the how-to-get-there page uses header images at this time as other region
+  # pages use manually entered HTML and images.
+  def region_header_urls
+    if @region && controller.action_name == 'how_to_get_there'
+      region_images(@region)
+        .map{|img| "/regions/#{@region.name.parameterize}/headers/#{img}"}
+    end
+  end
+
+  def country_header_urls
+    if @country
+      urls = []
       if @country.image
         urls << @country.image.url
       end
@@ -74,10 +100,8 @@ module ResortsHelper
       @images.each do |img|
         urls << "/countries/#{@country.name.parameterize}/headers/#{img}"
       end
+      urls
     end
-
-    urls.map! {|u| u.gsub(' ', '%20')}
-    urls
   end
 
   # Returns a sub-directory containing a resort's header images based on the
@@ -91,7 +115,7 @@ module ResortsHelper
 
   def resort_images(resort, sub_dir)
     dir = "#{RESORTS_DIRECTORY}#{resort.name.parameterize}/#{sub_dir}"
-    @images = images_in_directory(dir)
+    images_in_directory(dir)
   end
 
   def country_images(country, sub_dir)
--- spec/helpers/resorts_helper_spec.rb
+++ spec/helpers/resorts_helper_spec.rb
@@ -16,4 +16,59 @@ RSpec.describe ResortsHelper, type: :helper do
       expect(images_in_directory('nonexistent')).to eq []
     end
   end
+
+  describe '#header_image_urls' do
+    before do
+      assign(:region, region)
+    end
+
+    context 'with only region set' do
+      let(:region) { FactoryGirl.build(:region) }
+
+      it 'returns header image URLs for regions' do
+        urls = ['/path/to/r1.jpg']
+        expect(helper).to receive(:region_header_urls).and_return urls
+        expect(helper.header_image_urls).to eq urls
+      end
+    end
+  end
+
+  describe '#region_header_urls' do
+    before do
+      assign(:region, region)
+    end
+
+    context 'when @region set' do
+      let(:region) { FactoryGirl.build(:region) }
+
+      before do
+        allow(helper).to receive(:region_images).and_return ['r1.jpg']
+        allow(controller).to receive(:action_name).and_return action_name
+      end
+
+      context 'when on how-to-get-there page' do
+        let(:action_name) { 'how_to_get_there' }
+
+        it 'returns an array of region header URLs' do
+          expect(helper.region_header_urls).to eq ["/regions/#{region.name.parameterize}/headers/r1.jpg"]
+        end
+      end
+
+      context 'when on other pages' do
+        let(:action_name) { 'show' }
+
+        it 'returns nil' do
+          expect(helper.region_header_urls).to be_nil
+        end
+      end
+    end
+
+    context 'when @region not set' do
+      let(:region) { nil }
+
+      it 'returns nil' do
+        expect(helper.region_header_urls).to be_nil
+      end
+    end
+  end
 end
