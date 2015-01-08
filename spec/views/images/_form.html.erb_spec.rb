599ca1378f689cc1374148604270ba1a01330165
Elite <elicloudh@gmail.com>
2015-01-08 09:13:07 +0000

    State when no images have been uploaded

---
+++ spec/views/images/_form.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'images/_form.html.erb', type: :view do
+  let(:user) { FactoryGirl.create(:user) }
+
+  before do
+    assign(:image, Image.new)
+    assign(:object, object)
+    assign(:current_user, user)
+  end
+
+  context 'with a Property object' do
+    let(:object) { FactoryGirl.create(:property) }
+
+    context 'with no images uploaded' do
+      it 'states no images have been uploaded' do
+        render
+        expect(rendered).to have_content t('images.form.none_uploaded')
+      end
+    end
+  end
+end
