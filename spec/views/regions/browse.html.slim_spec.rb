e4b9a5ac275b012bcfae827154a7e74a252a9722
Elite <elicloudh@gmail.com>
2015-01-14 11:42:37 +0000

    Fall back to region theme on property browse, resolves #412

---
+++ spec/views/regions/browse.html.slim_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'properties/browse.html.slim', type: :view do
+  before do
+    assign(:breadcrumbs, {})
+    assign(:heading, 'heading')
+    assign(:search_filters, [])
+    assign(:properties, [])
+  end
+
+  context 'with @region' do
+    it "uses the region's theme" do
+      ski_region = FactoryGirl.build(:region)
+      assign(:region, ski_region)
+      allow(ski_region).to receive(:theme).and_return 'ski'
+      render
+      expect(view.content_for(:theme)).to eq 'ski'
+    end
+  end
+end
