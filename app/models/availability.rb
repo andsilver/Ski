52b2457519f981bff6bf18e9e59a1f5f41f7f980
Elite <elicloudh@gmail.com>
2015-02-23 13:34:14 +0000

    Update #cache_availability

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -6,4 +6,14 @@ class Availability < ActiveRecord::Base
   ]
 
   validates_inclusion_of :availability, in: AVAILABILITIES
+
+  # Converts an availability code from an <tt>InterhomeVacancy</tt> into an
+  # appropriate value for <tt>availability</tt>.
+  def self.availability_from_interhome(availability_code)
+    {
+      'N' => UNAVAILABLE,
+      'Y' => AVAILABLE,
+      'Q' => ENQUIRE
+    }[availability_code]
+  end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -268,12 +268,16 @@ class Property < ActiveRecord::Base
   end
 
   def cache_availability(dates)
-    return unless interhome_accommodation
+    return unless interhome_accommodation && interhome_accommodation.interhome_vacancy
 
     dates.each do |date|
-      if interhome_accommodation.available_to_check_in_on_dates?([date])
-        Availability.create!(property_id: id, start_date: date)
-      end
+      Availability.create!(
+        property_id: id,
+        start_date: date,
+        check_in: interhome_accommodation.check_in_on?(date),
+        check_out: interhome_accommodation.check_out_on?(date),
+        availability: Availability.availability_from_interhome(interhome_accommodation.availability_on(date))
+      )
     end
   end
 
