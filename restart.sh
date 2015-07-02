416446f8217da04c2c65b6d39cc7fca8a4ac7627
Elite <elicloudh@gmail.com>
2015-07-02 11:49:53 +0100

    Use four delayed_job workers

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-RAILS_ENV=production bin/delayed_job -n 3 stop
+RAILS_ENV=production bin/delayed_job -n 4 stop
 touch tmp/restart.txt
-RAILS_ENV=production bin/delayed_job -n 3 start
+RAILS_ENV=production bin/delayed_job -n 4 start
