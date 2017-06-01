d31239404e7ffcd5fd9e2c0c4847e2fce6945632
Elite <elicloudh@gmail.com>
2017-06-01 23:58:25 +0100

    Use Chrome for JS specs

--- spec/features/quick_search_spec.rb
+++ spec/features/quick_search_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
-feature 'Quick search' do
+RSpec.feature 'Quick search' do
   background do
     FactoryGirl.create(:website)
   end
 
   scenario 'Resort or region is required', js: true do
-    skip 'Flaky'
     visit '/favourites'
     click_button 'Search'
     accept_prompt 'Please choose a resort first.'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,6 +74,12 @@ end
 FactoryGirl.reload
 load "#{Rails.root}/config/routes.rb"
 
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.javascript_driver = :chrome
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
