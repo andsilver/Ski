a2f668aa263c52a48ea9983de6f200e04061962d
Elite <elicloudh@gmail.com>
2017-06-10 16:49:15 +0100

    Implement TripAdvisor::LocationImporter

---
+++ lib/trip_advisor/location_importer.rb
@@ -0,0 +1,34 @@
+module TripAdvisor
+  class LocationImporter
+    attr_reader :io
+
+    def initialize(io)
+      @io = io
+    end
+
+    # Performs importing of the data.
+    def import
+      data = JSON.parse(io.read)
+
+      import_location(nil, data)
+    end
+
+    # Imports a location and its children. Associates the location with its
+    # parent.
+    def import_location(parent_id, data)
+      # TripAdvisor locations start at 0. Since we're using MySQL, a 0 primary
+      # key would be considered an instruction to use the auto increment value
+      # by default. So we add one instead.
+      id = data['tripadvisorLocationId'] + 1
+      TripAdvisorLocation.create!(
+        id: id,
+        name: data['name'],
+        location_type: data['type'] || 'Earth',
+        parent_id: parent_id
+      )
+      if (children = data['children'])
+        children.each { |c| import_location(id, c) }
+      end
+    end
+  end
+end
---
+++ spec/lib/trip_advisor/location_importer_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+module TripAdvisor
+  RSpec.describe LocationImporter do
+    describe '#import' do
+      def json
+        %{
+          {
+            "tripadvisorLocationId" : 0,
+            "name" : "Earth",
+            "children" : [ {
+              "tripadvisorLocationId" : 6,
+              "name" : "Africa",
+              "type" : "continent"
+            }, {
+              "tripadvisorLocationId" : 2,
+              "name" : "Asia",
+              "type" : "continent"
+            } ]
+          }
+        }
+      end
+
+      before do
+        io = StringIO.new(json)
+        importer = LocationImporter.new(io)
+        importer.import
+      end
+
+      it 'creates a TripAdvisorLocation for each entry' do
+        expect(TripAdvisorLocation.count).to eq 3
+      end
+
+      it 'sets a name for each location' do
+        expect(TripAdvisorLocation.first.name).to eq 'Earth'
+      end
+
+      it 'sets a location_type for each location' do
+        expect(TripAdvisorLocation.last.location_type).to eq 'continent'
+      end
+
+      it 'sets a "planet" as the location_type for Earth' do
+        expect(TripAdvisorLocation.last.location_type).to eq 'continent'
+      end
+
+      it 'sets the id as 1 + tripadvisorLocationId, as latter starts at 0' do
+        expect(TripAdvisorLocation.find_by(name: 'Africa').id).to eq 7
+      end
+
+      it "sets the parent_id as 1 + the parent's tripadvisorLocationId" do
+        expect(TripAdvisorLocation.find_by(name: 'Africa').parent_id).to eq 1
+      end
+    end
+  end
+end
