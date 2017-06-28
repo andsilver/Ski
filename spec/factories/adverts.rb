4cc4d951818a4a427ac1ca9f4ad1a1267865ff69
Elite <elicloudh@gmail.com>
2017-06-28 19:47:21 +0100

    Fix link to show category page from directory advert

---
+++ spec/factories/adverts.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :advert do
+    association :user
+  end
+end
---
+++ spec/requests/directory_adverts_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Directory adverts', type: :request do
+  before { FactoryGirl.create(:website) }
+  describe 'GET /directory_adverts/:id' do
+    let!(:ad) { FactoryGirl.create(:directory_advert) }
+    def perform
+      get "/directory_adverts/#{ad.id}"
+    end
+
+    context 'advert is currently advertised' do
+      before do
+        FactoryGirl.create(
+          :advert, directory_advert_id: ad.id, expires_at: Date.tomorrow
+        )
+      end
+
+      it 'responds 200' do
+        perform
+        expect(response.status).to eq 200
+      end
+
+      it 'links to the category page' do
+        perform
+        assert_select(
+          "a[href='/categories/#{ad.category.to_param}/#{ad.resort.slug}']"
+        )
+      end
+    end
+  end
+end
