14867b505c6fea2139f9f8d8ba4c78ea5bf30481
Elite <elicloudh@gmail.com>
2017-07-15 13:19:22 +0100

    Implement TripAdvisor::PropertyFileImporter

---
+++ lib/trip_advisor/property_file_importer.rb
@@ -0,0 +1,15 @@
+module TripAdvisor
+  # Imports TripAdvisor properties (listings) from a file on the local
+  # filesystem.
+  class PropertyFileImporter
+    attr_reader :path
+
+    def initialize(path:)
+      @path = path
+    end
+
+    def import
+      File.foreach(path) { |json_line| PropertyImporter.new(json_line).import }
+    end
+  end
+end
---
+++ spec/lib/trip_advisor/property_file_importer_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+module TripAdvisor
+  RSpec.describe PropertyFileImporter do
+    describe '#import' do
+      it 'opens the file at path and asks PropertyImporter to import each ' \
+      'line of JSON' do
+        path = 'trip_advisor/listings_delta_yyyymmdd.txt'
+
+        pi1 = instance_double(PropertyImporter)
+        pi2 = instance_double(PropertyImporter)
+
+        expect(File)
+          .to receive(:foreach).with(path)
+          .and_yield("{\"id\":1}\n").and_yield("{\"id\":2}\n")
+
+        expect(PropertyImporter)
+          .to receive(:new).with("{\"id\":1}\n").and_return(pi1)
+        expect(pi1).to receive(:import)
+        expect(PropertyImporter)
+          .to receive(:new).with("{\"id\":2}\n").and_return(pi2)
+        expect(pi2).to receive(:import)
+
+        sut = PropertyFileImporter.new(path: path)
+        sut.import
+      end
+    end
+  end
+end
