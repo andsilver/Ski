ec3a6c030c6b3467e2bb537d2ec07746da4a55a0
Elite <elicloudh@gmail.com>
2017-10-20 11:27:01 +0100

    Add model for TripAdvisor CalendarEntry

---
+++ spec/factories/trip_advisor_calendar_entries.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+FactoryGirl.define do
+  factory :trip_advisor_calendar_entry do
+    trip_advisor_property_id 1
+    status "MyString"
+    inclusive_start "2017-10-20"
+    exclusive_end "2017-10-21"
+  end
+end
