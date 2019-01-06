27f932254a1a791da3bf3f22231a7978795d71b5
EL <elicloudh@gmail.com>
2019-01-06 14:43:01 +0000

    rentals homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,114 +30,132 @@ website = Website.create!(
   <div id="col-mid" class="wide clear-fix">
 
       	<div id="welcome-message">
-          <h1 class="home">Welcome to Mychaletfinder®</h1>
-          <p>One of Europe's leading self catering holiday accommodation
-          website offering <strong>ski chalets</strong>, apartments,
-          <strong>villas</strong> and holiday homes for rent.</p>
+          <h1 class="home">Ski Chalet Rentals</h1>
+          <p>Welcome to MyChaletFinder, the ultimate ski holiday rental website for mountain enthusiasts.</p>
           <br>
-          <p>We have a large selection of <strong>holiday
-          rentals</strong> in top destinations
-          around the world to suit all budgets
-          and tastes.</p>
+          <p>Book your ski chalet, cabin, condo or lodge today and enjoy the freedom,</p>
+          <p>flexibility and space to do what you want, when you want - home from home.</p>
           <br>
-
-          <p>Whether a city centre apartment, a cabin in Lapland, beach villa
-          in the Caribbean or ski chalet in the lakes &amp; mountains, MyChaletfinder has the perfect <strong>vacation
-          rental</strong> waiting for you.
-          </p>
+          <br>
         </div>
 
       	<div id="featured-properties">
-          <h2>Popular Holiday Destinations</h2>
-
-          <div class="clear-fix">
-
-            <a href="/resorts/chamonix/properties/rent" class="size-1of3 layout-element">
-              <img src="/countries/home-page/destinations/chamonix-chalets.jpg" alt="chalet rentals" title="Chamonix summer holidays">
-              <p class="header-title">Chamonix</p>
-
-              <div class="header-text-cont">
-                <p class="header-text">France</p>
+          <h2>Popular ski holiday destinations</h2>
+          <div class="preview-layout">
+            <div class="preview-item">
+              <img src="/assets/home/st_anton_austria.png">
+              <div class="overlay">
+                <h3 class="title">ST ANTON AUSTRIA</h3>
               </div>
-            </a>
-
-                 <a href="/regions/lake-como" class="lakes-and-mountains-theme size-1of3 layout-element">
-              <img src="/countries/home-page/destinations/lake-como-villa-rentals.jpg" alt="lakes and mountains chalet holiday rentals" title="Lake Como villas">
-              <p class="header-title">Lake Como</p>
-
-              <div class="header-text-cont">
-                <p class="header-text">Italy</p>
+            </div>
+            <div class="preview-item">
+              <img src="/assets/home/ziller_valley_austria.png">
+              <div class="overlay">
+                <h3 class="title">ZILLER VALLEY AUSTRIA</h3>
+                <div class="description">(81 PROPERTIES)</div>
               </div>
-            </a>
+            </div>
+            <div class="preview-item">
+              <img src="/assets/home/ruka_kuusamo_finland.png">
+              <div class="overlay">
+                <h3 class="title">RUKA-KUUSAMO FINLAND</h3>
+              </div>
+            </div>
+            <div class="preview-item">
+              <img src="/assets/home/chamonix_france.png">
+              <div class="overlay">
+                <h3 class="title">CHAMONIX FRANCE</h3>
+              </div>
+            </div>
+            <div class="preview-item">
+              <img src="/assets/home/verbier_switzerland.png">
+              <div class="overlay">
+                <h3 class="title">VERBIER Switzerland</h3>
+              </div>
+            </div>
+            <div class="preview-item">
+              <img src="/assets/home/lenk_switzerland.png">
+              <div class="overlay">
+                <h3 class="title">LENK Switzerland</h3>
+              </div>
+            </div>
+            <div class="preview-item">
+              <img src="/assets/home/bormio_italy.png">
+              <div class="overlay">
+                <h3 class="title">BORMIO Italy</h3>
+              </div>
+            </div>
+            <div class="preview-item">
+              <img src="/assets/home/canazei_italy.png">
+              <div class="overlay">
+                <h3 class="title">CANAZEI Italy</h3>
+              </div>
+            </div>
+            <div class="preview-item">
+              <img src="/assets/home/salen_sweden.png">
+              <div class="overlay">
+                <h3 class="title">SALEN Sweden</h3>
+              </div>
+            </div>
           </div>
-          <!-- clearfix 1of 3 row -->
-          <div class="clear-fix">
-            <a href="/regions/puglia" class="summer-villas-theme size-1of3 layout-element">
-              <img src="/countries/home-page/destinations/puglia-holiday-rentals.jpg" alt="puglia holiday rentals" title="Puglia holiday rentals">
-              <p class="header-title">Puglia</p>
-
-              <div class="header-text-cont">
-                <p class="header-text">Italy</p>
+        </div>
+        <br>
+        <br>
+        <div id="list-properties">
+          <p>
+            <img alt="" src="/assets/marker-blue-6ab58c513405cdba0d8e092cda36219911a882653a9822f9559a58ade5d375f3.svg">
+          </p>
+          <h2>List your Ski Chalet</h2>
+          <br>
+          <p>Ski holiday rentals are getting more popular with travellers from families to adventurers.</p>
+          <p>List your chalet on MyChaletFinder today!</p>
+          <br>
+          <br>
+          <a class="button action-button">List your property</a>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div id="ski-holiday-experiences">
+          <h2>Ski Holiday Experiences</h2>
+          <div class="preview-layout">
+            <div class="preview-item">
+              <img src="/assets/home/heli-skiing.png">
+              <div class="overlay">
+                <h3 class="title">HELI-SKIING</h3>
               </div>
-            </a>
-
-
-            <a href="/resorts/madrid" class="city-breaks-theme size-1of3 layout-element">
-              <img src="/countries/home-page/destinations/madrid-apartments.jpg" alt="madrid apartments" title="Madrid apartments">
-              <p class="header-title">Madrid</p>
-
-              <div class="header-text-cont">
-                <p class="header-text">Spain</p>
+            </div>
+            <div class="preview-item">
+              <img src="/assets/home/gastronomic_delights.png">
+              <div class="overlay">
+                <h3 class="title">GASTRONOMIC DELIGHTS</h3>
               </div>
-            </a>
-          </div>
-          <!-- clearfix 1of 3 row -->
-
-               <div class="clear-fix">
-
-
-
-            <a href="/regions/lake-tahoe" class="size-1of3 layout-element">
-              <img src="/countries/home-page/destinations/lake-tahoe-cabin-rentals.jpg" alt="vacation rentals" title="Lake Tahoe cabins">
-              <p class="header-title">Lake Tahoe</p>
-
-              <div class="header-text-cont">
-                <p class="header-text">America</p>
+            </div>
+            <div class="preview-item">
+              <img src="/assets/home/spa_wellbeing.png">
+              <div class="overlay">
+                <h3 class="title">SPA & WELLBEING</h3>
               </div>
-            </a>
-
-                 <a href="/resorts/zell-am-see-kaprun" class="lakes-and-mountains-theme size-1of3 layout-element">
-              <img src="/countries/home-page/destinations/zell-am-see-chalet-rentals.jpg" alt="lakes and mountains holiday rentals" title="Zell am See chalets">
-              <p class="header-title">Zell am See</p>
-
-              <div class="header-text-cont">
-                <p class="header-text">Austria</p>
+            </div>
+            <div class="preview-item">
+              <img src="/assets/home/ski_tours.png">
+              <div class="overlay">
+                <h3 class="title">SKI TOURS</h3>
               </div>
-            </a>
-          </div>
-          <!-- clearfix 1of 3 row -->
-
-
-
-          <div class="clear-fix">
-            <a href="/pages/caribbean-holidays" class="summer-villas-theme size-1of3 layout-element">
-              <img src="/countries/home-page/destinations/caribbean-vacation-rentals.jpg" alt="caribbean vacation rentals, villas to rent" title="Caribbean holiday rentals">
-              <p class="header-title">British Virgin Islands</p>
-
-              <div class="header-text-cont">
-                <p class="header-text">Caribbean</p>
+            </div>
+            <div class="preview-item">
+              <img src="/assets/home/beginner_resorts.png">
+              <div class="overlay">
+                <h3 class="title">BEGINNER RESORTS</h3>
               </div>
-            </a>
-
-
-            <a href="/resorts/vancouver" class="city-breaks-theme size-1of3 layout-element">
-              <img src="/countries/home-page/destinations/vancouver-vacation-rentals.jpg" alt="vancouver vacation rentals" title="Vancouver vacation rentals">
-              <p class="header-title">Vancouver</p>
-
-              <div class="header-text-cont">
-                <p class="header-text">Canada</p>
+            </div>
+            <div class="preview-item">
+              <img src="/assets/home/eco_friendly.png">
+              <div class="overlay">
+                <h3 class="title">ECO FRIENDLY</h3>
               </div>
-            </a>
+            </div>
           </div>
         </div>
 
