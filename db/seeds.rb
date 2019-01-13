b719148d4f15a3529179cb11de062243e033fb3e
EL <elicloudh@gmail.com>
2019-01-13 19:13:12 +0000

    greenland update

--- db/seeds.rb
+++ db/seeds.rb
@@ -181,92 +181,77 @@ Page.create!([
     title: 'Greenland Heli-skiing | Ski Tours Kangaamiut, Greenland',
     content: <<~EOF
     <div class="wide clear-fix experience-details">
-    <ul class="experience-slide">
-        <li>
-            <img src="/assets/home/greenland-glacier-5.jpg" alt="">
-        </li>
-        <li>
-            <img src="/assets/home/greenland-iceberg-sunset-4jpg.jpg" alt="">
-        </li>
-        <li>
-            <img src="/assets/home/greenland-northern-lights-1.jpg" alt="">
-        </li>
-    </ul>
     <div>
-        <br>
-        <br>
-        <h2 style="text-align: center">G R E E N L A N D</h2>
-        <br>
-        <div>
-            <h1>Maniitsoq</h1>
-            <div class="flex-layout">
-            <div class="flex-2" style="margin-right: 24px">
-                <p>Maniitsoq is an archipelago of islands, glaciers and fjords on the South West side of Greenland.</p>
+        <div class="page-description">
+            <img src="/assets/home/maniitsoq-heli-skiing.jpg" alt="" width="100%">
+            <br>
+            <br>
+            <br>
+            <h1>Greenland Heli-skiing and Ski Tours</h1>
+            <h2>Wild & remote - Spectacular Scenery, Icebergs, Glaciers and Fjords</h2>
+            <br>
+            <p>Everyone has heard of heli-skiing in Canada - afterall, it is the world's heli-skiing capital with around 80% of the market for this adrenaline fuelled sport. However,as the lesser known mountainous nations have started to promote their national assets ie mountains and ice, the areas often thought as being too remote are proving to be just as good and better value than the established players - Greenland, the world's largest island, is no exception.</p>
+        </div>
+        <div class="flex-layout content-space-between">
+            <div class="flex-2">
+                <img src="/assets/home/glacier-skiing-greenland.jpg" alt="" width="100%">
+            </div>
+            <div class="flex-2">
+                <p>Some of the best heli-skiing can be found on the many islands surrounding Greenland's huge coastline. These regions often have the deepest fjords with many permanently covered by glacial ice. On some of the decents, you can ski from the drop-off point, right the way down to the waters edge - something you won't experience when heli-skiing in the Alps.</p>
                 <br>
-                <p>There is huge variety and quality of terrain with drop-offs from the top of the Ice Cap to the beach! Adam peak on the edge of Eternity Fjord is the highest mountain in the area at 2000m and together with the other heli-ski drop-off points, give vertical decents ranging from 600 to 1500m.</p>
-                <h2>Getting there</h2>
-                <p>Fly to Kangerlussuaq International and then take the 40 minute flight to Maniitsoq.</p>
-            </div>
-            <div class="flex-2">
-                <img src="/assets/home/maniitsoq-heli-skiing-ledge12.jpg" alt="" width="100%">
-            </div>
-            </div>
-        </div>
-        <br>
-        <div>
-            <h1>Kangaamiut</h1>
-            <div class="flex-layout">
-            <div class="flex-2" style="margin-right: 24px">
-                <p>Kangaamiut is a tiny island off the west coast and is used as a base for the heli-skiing operators. The area is packed with literally hundreds of glacial runs with 2000m decents that end up at the edge of the 3 fjords that surround this remote island.</p>
-                <br>
-                <p>If you like "remote", this is probably as good as it gets without visiting the polar ice caps!</p>
-                <h2>Getting there</h2>
-                <p>The closest domestic airport is Maniitsoq which is just 50km away. However, there are no roads to Kangaamiut from Maniitsoq so you can either catch a boat or take a helicopter to the island. If you are travelling on a tour, the onwards transpot from Maniitsoq airport will be provided.</p>
-            </div>
-            <div class="flex-2">
-                <img src="/assets/home/kangaamiut-heli-skiing-greenland.jpg" alt="" width="100%">
-            </div>
-            </div>
-        </div>
-        <br>
-        <div>
-            <h1>Kulusuk</h1>
-            <div class="flex-layout">
-            <div class="flex-2" style="margin-right: 24px">
-                <p>Kulusuk is the gateway to the East coast. Its a small settlement in the Ammassalik region of Greenland built on a rocky island typically surrounded by fjords and mountains! From the top of the local Isikajia Mountain at 300m, you can see the huge icebergs that normally surround the island and beyond in to the Danish Straits.</p>
-                <h2>Getting there</h2>
-                <p>Kulusuk airport is just 40 minutes flying time from Kangerlussuaq International. Alternatively, its about a 2 hour flight from the capital, Nuuk.</p>
-            </div>
-            <div class="flex-2">
-                <img src="/assets/home/kusuluk-panorama-9.jpg" alt="" width="100%">
-            </div>
+                <h2>Greenland - facts & figures</h2>
+                <div style="border-top: 1px solid #ececec; padding: 24px 0;">
+                    <table style="width: 100%">
+                        <tbody>
+                            <tr>
+                                <td class="row-divided" style="width: 50%;">Heli skiing main season</td>
+                                <td class="row-divided">April to Mid May</td>
+                            <tr>
+                                <td class="row-divided">Northern Lights</td>
+                                <td class="row-divided">Dec to March</td>
+                            </tr>
+                            <tr>
+                                <td class="row-divided">Midnight Sun</td>
+                                <td class="row-divided">Mid May to the end of July</td>
+                            </tr>
+                            <tr>
+                                <td class="row-divided">Capital</td>
+                                <td class="row-divided">Nuuk</td>
+                            </tr>
+                            <tr>
+                                <td class="row-divided">Currency</td>
+                                <td class="row-divided">Danish Kroner</td>
+                            </tr>
+                            <tr>
+                                <td class="row-divided">Time (Nuuk)</td>
+                                <td class="row-divided">GMT -3 hours</td>
+                            </tr>
+                            <tr>
+                                <td class="row-divided">More info</td>
+                                <td class="row-divided">www.greenland.com</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
         <br>
-        <div>
-            <h1>Uummannaq</h1>
-            <div class="flex-layout">
-            <div class="flex-2" style="margin-right: 24px">
-                <p>Uummannaq is a small village of nearly 1300 residents on an island 500km north of the Arctic Circle. Being so far north, daylight hours in the peak winter months are minimal. However, from mid May to mid August, its the complete opposite with the Midnight Sun from mid May to the end of July. This means heli-skiing is possible nearly 24 hours a day and there is nothing better than skiing virgin snow under an orange sky!</p>
-                <br>
-                <p>One of the best places for heli-skiing is on the Sernersuaq glacier on the Nuussuaqq peninsula.</p>
-                <h2>Getting there</h2>
-                <p>From Kangerlussuaq International airport, catch a flight to Qaarsut and from there, its a 10 minute ride in a helicopter or you can drive.</p>
-            </div>
-            <div class="flex-2">
-                <img src="/assets/home/uummannaq-heli-skiing-11.jpg" alt="" width="100%">
-            </div>
-            </div>
-        </div>
+        <p style="text-align: center">
+            When deciding on a heli-skiing adventure in Greenland, you have a choice of several areas each having its own unique characteristics. And, if you are planning on visiting several areas on your heli-skiing trip, its worth noting that the roads end where a city or town perimeter ends. After that, its snow, ice, mountains and fjords so transport is by boat, plane or helicopter.
+        </p>
     </div>
-    <div style="margin-top: 20px;">
+    <br>
+    <br>
+    <br>
+    <br>
+    <div>
         <ul id="tabs">
             <li><a id="tab1">OVERVIEW</a></li>
             <li><a id="tab2">OPERATORS</a></li>
         </ul>
         <div class="tab-container" id="tab1C">
-            <div class="flex-layout content-space-between">
-                <div class="flex-2" style="margin-right: 24px">
+            <div class="flex-layout">
+                <div class="flex-2">
                     <p>With fjords and glaciers everywhere, the scenery is spectacular and being north of the Arctic Circle means long days from April onwards - which coincides with the start of the heli-skiing season!</p>
                     <br>
                     <p>The north slopes offer powder whilst south facing slopes deliver spring com snow. The terrain is varied from short stepp couloirs to traversing across mighty glaciers with many runs ending up at the beach!</p>
@@ -283,6 +268,7 @@ Page.create!([
                 </div>
                 <div class="flex-2" style="font-size: 14px">
                     <div class="preview-map" style="width:100%; height: 250px;"></div>
+                    <br>
                     <br>
                     <iframe src="https://www.meteoblue.com/en/weather/widget/daily/nuuk_greenland_3421319?days=5&amp;tempunit=CELSIUS&amp;windunit=MILE_PER_HOUR&amp;pictoicon=0&amp;pictoicon=1&amp;maxtemperature=0&amp;maxtemperature=1&amp;mintemperature=0&amp;mintemperature=1&amp;windspeed=0&amp;windgust=0&amp;winddirection=0&amp;uv=0&amp;humidity=0&amp;precipitation=0&amp;precipitation=1&amp;precipitationprobability=0&amp;spot=0&amp;pressure=0&amp;layout=light" scrolling="NO" allowtransparency="true" style="width: 100%;height:300px"></iframe>
                     <br>
@@ -304,7 +290,7 @@ Page.create!([
             <div>
                 <h3>Adventures</h3>
                 <div class="flex-layout">
-                    <div class="flex-2" style="margin-right: 24px">
+                    <div class="flex-2">
                         <p>And when you are not heli-skiing, there are are many natural wonders everywhere from the fjords and icebergs to the amazing northern lights (from Dec to March). Also possible on down days, if the weather is too bad for the helicopter, you can try...</p>
                         <ul style="list-style: inherit; padding-left: 18px;">
                             <li><p>Snowmobile safari</p></li>
@@ -325,8 +311,8 @@ Page.create!([
             <h1 style="text-align: center">Greenland heli Skiing.com</h1>
             <br>
             <br>
-            <div class="flex-layout content-space-between">
-                <div class="flex-2" style="margin-right: 24px">
+            <div class="flex-layout">
+                <div class="flex-2">
                     <p>Come ski the wild terrain of West Greenland. With 2000m runs to the fjord''s edge, deep powder and corn snow, our heli-skiing trps give you the experience of a life time.</p>
                     <br>
                     <p>We base our tours 60km north of Maniitsoq, in the tiny village of Kangaamiut, where we stay in rented accommodation and experience life as the locals do.</p>
@@ -340,7 +326,7 @@ Page.create!([
                     <div style="border-top: 1px solid #ececec; border-bottom: 1px solid #ececec; padding: 24px 0; margin:24px 0">
                         <table>
                             <tbody>
-                                <tr>
+                                <tr >
                                     <td style="width: 167px;">BASE</td>
                                     <td><p>Kangaamuit Island</p></td>
                                 <tr>
@@ -383,9 +369,89 @@ Page.create!([
                         </table>
                     </div>
                     <div>
-                        <a class="button action-button" style="padding: 1rem 5rem">ENQUIRE</a>
+                        <a class="button action-button" href="http://www.greenlandheliskiing.com/Greenland_Heli_Skiing.html" style="padding: 1rem 5rem">ENQUIRE</a>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    <h2 style="text-align: center">G R E E N L A N D</h2>
+    <br>
+    <br>
+    <ul class="experience-slide">
+        <li>
+            <img src="/assets/home/greenland-glacier-5.jpg" alt="">
+        </li>
+        <li>
+            <img src="/assets/home/greenland-iceberg-sunset-4jpg.jpg" alt="">
+        </li>
+        <li>
+            <img src="/assets/home/greenland-northern-lights-1.jpg" alt="">
+        </li>
+    </ul>
+    <br>
+    <br>
+    <div>
+        <div>
+            <h1>Maniitsoq</h1>
+            <div class="flex-layout">
+            <div class="flex-2">
+                <p>Maniitsoq is an archipelago of islands, glaciers and fjords on the South West side of Greenland.</p>
+                <br>
+                <p>There is huge variety and quality of terrain with drop-offs from the top of the Ice Cap to the beach! Adam peak on the edge of Eternity Fjord is the highest mountain in the area at 2000m and together with the other heli-ski drop-off points, give vertical decents ranging from 600 to 1500m.</p>
+                <h2>Getting there</h2>
+                <p>Fly to Kangerlussuaq International and then take the 40 minute flight to Maniitsoq.</p>
+            </div>
+            <div class="flex-2">
+                <img src="/assets/home/maniitsoq-heli-skiing-ledge12.jpg" alt="" width="100%">
+            </div>
+            </div>
+        </div>
+        <br>
+        <div>
+            <h1>Kangaamiut</h1>
+            <div class="flex-layout">
+            <div class="flex-2">
+                <p>Kangaamiut is a tiny island off the west coast and is used as a base for the heli-skiing operators. The area is packed with literally hundreds of glacial runs with 2000m decents that end up at the edge of the 3 fjords that surround this remote island.</p>
+                <br>
+                <p>If you like "remote", this is probably as good as it gets without visiting the polar ice caps!</p>
+                <h2>Getting there</h2>
+                <p>The closest domestic airport is Maniitsoq which is just 50km away. However, there are no roads to Kangaamiut from Maniitsoq so you can either catch a boat or take a helicopter to the island. If you are travelling on a tour, the onwards transpot from Maniitsoq airport will be provided.</p>
+            </div>
+            <div class="flex-2">
+                <img src="/assets/home/kangaamiut-heli-skiing-greenland.jpg" alt="" width="100%">
+            </div>
+            </div>
+        </div>
+        <br>
+        <div>
+            <h1>Kulusuk</h1>
+            <div class="flex-layout">
+            <div class="flex-2">
+                <p>Kulusuk is the gateway to the East coast. Its a small settlement in the Ammassalik region of Greenland built on a rocky island typically surrounded by fjords and mountains! From the top of the local Isikajia Mountain at 300m, you can see the huge icebergs that normally surround the island and beyond in to the Danish Straits.</p>
+                <h2>Getting there</h2>
+                <p>Kulusuk airport is just 40 minutes flying time from Kangerlussuaq International. Alternatively, its about a 2 hour flight from the capital, Nuuk.</p>
+            </div>
+            <div class="flex-2">
+                <img src="/assets/home/kusuluk-panorama-9.jpg" alt="" width="100%">
+            </div>
+            </div>
+        </div>
+        <br>
+        <div>
+            <h1>Uummannaq</h1>
+            <div class="flex-layout">
+            <div class="flex-2">
+                <p>Uummannaq is a small village of nearly 1300 residents on an island 500km north of the Arctic Circle. Being so far north, daylight hours in the peak winter months are minimal. However, from mid May to mid August, its the complete opposite with the Midnight Sun from mid May to the end of July. This means heli-skiing is possible nearly 24 hours a day and there is nothing better than skiing virgin snow under an orange sky!</p>
+                <br>
+                <p>One of the best places for heli-skiing is on the Sernersuaq glacier on the Nuussuaqq peninsula.</p>
+                <h2>Getting there</h2>
+                <p>From Kangerlussuaq International airport, catch a flight to Qaarsut and from there, its a 10 minute ride in a helicopter or you can drive.</p>
+            </div>
+            <div class="flex-2">
+                <img src="/assets/home/uummannaq-heli-skiing-11.jpg" alt="" width="100%">
+            </div>
             </div>
         </div>
     </div>
