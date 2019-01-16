abbfc800c970b81156a7e644c333b3610ecb58ba
EL <elicloudh@gmail.com>
2019-01-16 19:46:19 +0000

    rentals pages update

--- app/assets/javascripts/ui.settings.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('.experience-slide').partialViewSlider({
-    width: 50,
-    controls: true,
-    controlsPosition: 'inside',
-    backdrop: true,
-    dots: false
-  });
-});
