1e726faec20ef974f7b4c76da7925a9fa0b33991
Elite <elicloudh@gmail.com>
2019-04-17 09:44:59 +0100

    Fix show_classic spec; don't show capacity when for sale

--- spec/views/properties/show_classic.html.erb_spec.rb
+++ spec/views/properties/show_classic.html.erb_spec.rb
@@ -3,8 +3,9 @@ require "rails_helper"
 RSpec.describe "properties/show_classic", type: :view do
   it "shows a table of features" do
     assign(:property, FactoryBot.create(:property).decorate)
+    assign(:resort, FactoryBot.build(:resort))
     render
-    expect(rendered).to have_selector(".classic-images-features")
+    expect(rendered).to have_selector(".interhome-features")
   end
 
   context "when for rent" do
@@ -16,11 +17,12 @@ RSpec.describe "properties/show_classic", type: :view do
           listing_type: Property::LISTING_TYPE_FOR_RENT, sleeping_capacity: 3
         ).decorate
       )
+      assign(:resort, FactoryBot.build(:resort))
     end
 
     it "shows sleeping capacity" do
       render
-      expect(rendered).to have_content "Sleeping capacity: 3"
+      expect(rendered).to have_content "Capacity: 3"
     end
   end
 
@@ -33,11 +35,12 @@ RSpec.describe "properties/show_classic", type: :view do
           listing_type: Property::LISTING_TYPE_FOR_SALE, sleeping_capacity: 3
         ).decorate
       )
+      assign(:resort, FactoryBot.build(:resort))
     end
 
     it "does not show sleeping capacity" do
       render
-      expect(rendered).not_to have_content "Sleeping capacity"
+      expect(rendered).not_to have_content "Capacity"
     end
   end
 end
