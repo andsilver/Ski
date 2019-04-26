617f80203e1951df4cf0ba718a5d4c704dfd5bb3
Elite <elicloudh@gmail.com>
2019-04-26 19:10:49 +0100

    Use CMS for property sales page; resolves #659

---
+++ spec/requests/property_sales_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "Property sales", type: :request do
+  before { FactoryBot.create(:website) }
+
+  describe "GET /property_sales" do
+    it "renders content from the CMS" do
+      FactoryBot.create(
+        :page,
+        path: "/property_sales",
+        content: '<h1 class="home">International Property Sales</h1>'
+      )
+      get "/property_sales"
+      puts response.body
+      assert_select "h1.home", "International Property Sales"
+    end
+  end
+end
